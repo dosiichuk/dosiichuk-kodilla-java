ca1939eb779b647d5f674a191016f0ebc7806ce4
Dmytro Osiichuk <developer24contact@gmail.com>
2025-04-02 11:43:45 +0200

    15.6 SQL statements

--- kodilla-jdbc/src/test/java/com/kodilla/kodilla_jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/kodilla_jdbc/DbManagerTestSuite.java
@@ -2,8 +2,12 @@ package com.kodilla.kodilla_jdbc;
 
 import org.junit.jupiter.api.Test;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DbManagerTestSuite {
@@ -14,5 +18,55 @@ public class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
         //Then
         assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(5, counter);
+    }
+
+    @Test
+    void testSelectActiveUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String query = """
+                SELECT COUNT(*) AS POST_COUNT, USERS.FIRSTNAME, USERS.LASTNAME FROM POSTS
+                JOIN USERS ON POSTS.USER_ID = USERS.ID\s
+                GROUP BY USER_ID HAVING POST_COUNT > 1;
+                """;
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(query);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("POST_COUNT") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(2, counter);
     }
 }
